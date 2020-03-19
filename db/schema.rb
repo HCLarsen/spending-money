0cb99ac4c2ff63f1d7ff3e129cd1eaec36fefd2b
Chris Larsen <clarsendev@protonmail.com>
2020-03-18 23:50:19 -0400

    Add name field to devise model

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_184607) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "name"
-    t.string "nickname"
-    t.string "image"
     t.string "email"
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -4,6 +4,7 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   def setup
     @sign_up_url = '/api/v1/auth/'
     @signup_params = {
+        name: 'John Doe',
         email: 'john.doe@example.org',
         password: 'FakePassword123!',
         password_confirmation: 'FakePassword123!'
@@ -24,8 +25,6 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     post @sign_in_url, params: @login_params
     assert_response :success
     content = JSON.parse(response.body)
-    id = content["data"]["id"]
-    token = response.headers['access-token']
     assert response.headers['access-token']
   end
 
@@ -44,9 +43,34 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
       'expiry': headers['expiry'],
       'uid': headers['uid'],
     }
-    get '/api/v1/users/1', params: authenticate_params
+    get '/api/v1/users/1', headers: authenticate_params
     assert_response :success
     content = JSON.parse(response.body)
     assert_equal "john.doe@example.org", content["email"]
+    assert_equal "John Doe", content["name"]
+  end
+
+  test "should update user profile" do
+    post @sign_up_url, params: @signup_params
+    post @sign_in_url, params: @login_params
+    headers = response.headers
+
+    authenticate_params = {
+      'access-token': headers['access-token'],
+      'client': headers['client'],
+      'expiry': headers['expiry'],
+      'uid': headers['uid'],
+    }
+    data = {
+      'name': 'Johnathon Doe',
+    }
+    put '/api/v1/auth', headers: authenticate_params, params: data
+    assert_response :success
+
+    content = JSON.parse(response.body)
+
+    get '/api/v1/users/1', params: authenticate_params
+    content = JSON.parse(response.body)
+    assert_equal "Johnathon Doe", content["name"]
   end
 end
